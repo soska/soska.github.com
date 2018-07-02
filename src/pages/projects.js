c826557aa410351ed0bbee5f25d639e085e87ac5
Armando Sosa <asosa@nolimit-studio.com>
2018-07-02 17:54:52 -0500

    projects updated

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,10 @@ import Helmet from 'react-helmet';
 import { rhythm } from '../utils/typography';
 import gray from 'gray-percentage';
 
+import keyboardist from '../assets/keyboardist.png';
+import givemeacolor from '../assets/givemeacolor.png';
+import savage from '../assets/savage.png';
+
 const github = name => `https://github.com/soska/${name}`;
 
 const Project = ({ name, link, children }) => {
@@ -39,11 +43,22 @@ const Projects = () => {
       <Helmet title={`Projects | Armando Sosa`} />
       <h2>Open Source</h2>
       <Project name="Keyboardist" link={github('keyboardist.js')}>
+        <img src={keyboardist} alt="Keyboardist" />
         Easy way to add key shortcuts to your Javascript application.
       </Project>
       <Project name="React Keyboardist" link={github('react-keyboardist')}>
         React wrapper for Keyboardist
       </Project>
+      <h2>Fun Tools</h2>
+      <Project name="Give Me A Color" link={'https://givemeacolor.com'}>
+        <img src={givemeacolor} alt="GiveMeAColor.com" />
+        Sometimes one needs a random color to get the inspiration going.
+      </Project>
+      <Project name="Savage" link={'https://savage.now.sh'}>
+        <img src={savage} alt="GiveMeAColor.com" />
+        Quick way to turn an SVG file into a React Component.
+      </Project>
+
       {/*      <Project name="Unavatar" link={github('unavatar')}>
         React component to add alphanumeric avatars to users.
       </Project>
